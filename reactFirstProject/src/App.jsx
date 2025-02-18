34b36830acb0f505b4862dff589ff585ff0ef740
Ebru Bircan <ebrubircan2001@icloud.com>
2025-02-18 05:32:13 +0300

    Css düzenlemeleri

--- reactFirstProject/src/App.jsx
+++ reactFirstProject/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import 'bulma/css/bulma.css'
 import './App.css'
 import Course from './Course'
 import Angular from './assets/Angular.avif'
@@ -12,27 +12,45 @@ function App() {
   return (
     <>
       <div className='App'>
-
-        <Course 
-          image = {Angular}
-          title='Angular'
-          description='Angular is a platform and framework for building single-page client applications using HTML and TypeScript.'
-       />
-        <Course 
-          image = {Bootstrap}
-          title='Bootstrap'
-          description='Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development.'  
-        />
-        <Course 
-          image = {Vue}
-          title='Vue'
-          description='Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications.'
-        />
-        <Course
-          image = {Reactt}
-          title='React'
-          description='React is an open-source, front end, JavaScript library for building user interfaces or UI components.'
-        />
+      <section className="hero is-link">
+      <div className="hero-body">
+    <p className="title">Courses</p>
+  </div>
+</section>    
+      <div className='container'>
+        <section className="section">
+        <div className='columns'>
+          <div className='column'>
+            <Course 
+            image = {Angular}
+            title='Angular'
+            description='Angular is a platform and framework for building single-page client applications using HTML and TypeScript.'
+            />
+          </div>
+          <div className='column'>
+            <Course 
+            image = {Bootstrap}
+            title='Bootstrap'
+            description='Bootstrap is a free and open-source CSS framework directed at responsive, mobile-first front-end web development.'  
+            />
+          </div>
+          <div className='column'>
+            <Course 
+            image = {Vue}
+            title='Vue'
+            description='Vue.js is an open-source model–view–viewmodel front end JavaScript framework for building user interfaces and single-page applications.'
+            />
+          </div>
+          <div className='column'>
+            <Course
+            image = {Reactt}
+            title='React'
+            description='React is an open-source, front end, JavaScript library for building user interfaces or UI components.'
+            />
+          </div>
+        </div>
+        </section>
+      </div>
       </div>
     </>
   )
--- reactFirstProject/src/Course.jsx
+++ reactFirstProject/src/Course.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 function Course(props) {
     const { image, title, description } = props
     return (
-        <div>
-            <img src={image} alt='' />
-            <div>{title}</div>
-            <div>{description}</div>
-        </div>
+        <div className="card">
+  <div className="card-image">
+    <figure className="image is-4by3">
+      <img
+        src={image}
+        alt="Courses"
+      />
+    </figure>
+  </div>
+  <div className="card-content">
+    <div className="media">
+
+      <div className="media-content">
+        <p className="title is-4">{title}</p>
+      </div>
+    </div>
+
+    <div className="content">
+        {description}
+    </div>
+  </div>
+</div>
   );
 }
 
